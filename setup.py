906acabe44a9d156b52c796c6de74302875cd28a
weifei0228 <weifei0228@gmail.com>
2018-01-11 14:21:47 +0800

    test

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='YourAppName',
+setup(name='spidetest',
       version='1.0',
       description='OpenShift App',
       author='Your Name',
       author_email='example@example.com',
       url='http://www.python.org/sigs/distutils-sig/',
+	  packages=find_packages()
       install_requires=['Flask>=0.10.1','BeautifulSoup4'],
      )
